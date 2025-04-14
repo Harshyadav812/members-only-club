364ddba5ffcf01a9a8b91f8a3ed37a465fc6f238
Harshyadav812 <raoharsh138@gmail.com>
2025-04-14 10:18:14 +0000

    feat: initial Express server setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
