0fbb0a2be675c9e6aa7d3d9b775705909a48f451
Harshyadav812 <raoharsh138@gmail.com>
2025-05-11 12:43:15 +0000

    feat: implement membership status update and message creation functionality

--- db/queries.js
+++ db/queries.js
@@ -54,25 +54,25 @@ async function getUserbyId(id) {
   }
 }
 
-// async function updateMembershipStatus(userId) {
-//   try {
-//     const updateQuery = "UPDATE users SET is_member = true WHERE id = $1";
-//     await pool.query(updateQuery, [userId]);
-//   } catch (error) {
-//     console.error("Error updating membership status: ", error);
-//     throw error;
-//   }
-// }
+async function updateMembershipStatus(userId, isMember) {
+  try {
+    const updateQuery = "UPDATE users SET is_member = $1 WHERE id = $2";
+    await pool.query(updateQuery, [isMember, userId]);
+  } catch (error) {
+    console.error("Error updating membership status: ", error);
+    throw error;
+  }
+}
 
-// async function createMessage(userId, title, content) {
-//   try {
-//     const insertQuery = "INSERT INTO messages (user_id, title, content) VALUES ($1, $2, $3)";
-//     await pool.query(insertQuery, [userId, title, content]);
-//   } catch (error) {
-//     console.error("Error creating message: ", error);
-//     throw error;
-//   }
-// }
+async function createPost(userId, title, content) {
+  try {
+    const insertQuery = "INSERT INTO messages (user_id, title, content) VALUES ($1, $2, $3)";
+    await pool.query(insertQuery, [userId, title, content]);
+  } catch (error) {
+    console.error("Error creating message: ", error);
+    throw error;
+  }
+}
 
 // async function getAllMessages(isMember) {
 //   try {
@@ -96,11 +96,27 @@ async function getUserbyId(id) {
 //   }
 // }
 
+async function getAllMessages() {
+  try {
+    const query = `SELECT m.*, u.first_name, u.last_name
+    FROM messages m
+    JOIN users u ON m.user_id = u.id
+    ORDER BY m.created_at DESC`;
+    const result = await pool.query(query);
+    return result.rows;
+  }
+  catch (error) {
+    console.error("Error fetching messages: ", error);
+    throw error;
+  }
+}
+
+
 module.exports = {
   createUser,
   getUserByUsername,
   getUserbyId,
-  // updateMembershipStatus,
-  // createMessage,
-  // getAllMessages
+  updateMembershipStatus,
+  createPost,
+  getAllMessages
 };
